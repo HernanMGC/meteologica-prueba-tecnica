de3633522b3bd9c281069f58747a2fb560f438f6
errnan <hernan.gonzalez.calderon@gmail.com>
2025-11-03 10:26:58 +0100

    Moved from sqlite to mysql connection.

--- service_a/main.cpp
+++ service_a/main.cpp
@@ -6,6 +6,14 @@
 #include <ctime>
 #include <vector>
 
+#include "mysql_connection.h"
+#include "mysql_driver.h"
+#include <cppconn/driver.h>
+#include <cppconn/exception.h>
+#include <cppconn/resultset.h>
+#include <cppconn/statement.h>
+#include <cppconn/prepared_statement.h>
+
 enum EWeatherTableCols
 {
     DATE = 0,
@@ -18,37 +26,19 @@ enum EWeatherTableCols
 
 struct SWeatherLine
 {
-    std::string date;
+    std::time_t date;
 };
 
 int main() {
     crow::SimpleApp app;
 
-    sqlite3* db;
-    int rc = sqlite3_open("weather.db", &db);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Can't open database: " << sqlite3_errmsg(db) << std::endl;
-        return 1;
-    }
+    sql::mysql::MySQL_Driver* driver;
+    sql::Connection* con;
 
-    char* zErrMsg = nullptr;
-    rc = sqlite3_exec(db,
-        "CREATE TABLE IF NOT EXISTS weather ("
-        "id INTEGER PRIMARY KEY AUTOINCREMENT, "
-        "date DATE NOT NULL"
-        //", city TEXT NOT NULL"
-        //", temp_max DECIMAL(10, 5) NOT NULL"
-        //", temp_min DECIMAL(10, 5) NOT NULL"
-        //", precipitation DECIMAL(10, 5) NOT NULL"
-        //", cloudiness DECIMAL(10, 5) NOT NULL"
-        "); ",
-        nullptr, nullptr, &zErrMsg);
-    if (rc != SQLITE_OK) {
-        std::cerr << "SQL error: " << zErrMsg << std::endl;
-        sqlite3_free(zErrMsg);
-        sqlite3_close(db);
-        return 1;
-    }
+    driver = sql::mysql::get_mysql_driver_instance();
+    
+    con = driver->connect("mysql:3306", "root", "pass");
+    con->setSchema("weather_db");
 
     CROW_ROUTE(app, "/health")
     .methods("GET"_method)([]() {
@@ -58,10 +48,14 @@ int main() {
     });
 
     CROW_ROUTE(app, "/weather")
-    .methods("GET"_method)([&db]() {
+    .methods("GET"_method)([&con]() {
         std::vector<crow::json::wvalue> weather_lines;
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(db, "SELECT "
+        
+        sql::ResultSet *res;
+        sql::Statement *stmt;
+
+        stmt = con->createStatement();
+        res = stmt->executeQuery("SELECT "
             "id"
             ", date"
             // ", city"
@@ -69,14 +63,12 @@ int main() {
             // ", temp_min"
             // ", precipitation"
             // ", cloudiness"
-            " FROM weather", -1, &stmt, nullptr) != SQLITE_OK) {
-            return crow::response{500, "{\"error\": \"Database error\"}"};
-        }
-
-        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            " FROM weather"
+        ); // replace with your statement
+        while (res->next()) {
             crow::json::wvalue weather_line;
-            weather_line["id"] = sqlite3_column_int(stmt, 0);
-            weather_line["date"] = (const char*)sqlite3_column_text(stmt, 1);
+            weather_line["id"] = res->getInt(1);
+            weather_line["date"] = res->getString(2);
             // weather_line["city"] = (const char*)sqlite3_column_text(stmt, 2);
             // weather_line["temp_max"] = sqlite3_column_double(stmt, 3);
             // weather_line["temp_min"] = sqlite3_column_double(stmt, 4);
@@ -84,13 +76,14 @@ int main() {
             // weather_line["cloudiness"] = sqlite3_column_double(stmt, 6);
             weather_lines.push_back(weather_line);
         }
-        sqlite3_finalize(stmt);
+        delete res;
+        delete stmt;
 
         return crow::response{200, crow::json::wvalue{{"weather_lines", weather_lines}}};
     });
 
     CROW_ROUTE(app, "/weather")
-    .methods("POST"_method)([&db](const crow::request& req) {
+    .methods("POST"_method)([&con](const crow::request& req) {
         crow::multipart::message file_message(req);
         for (const auto& part : file_message.part_map)
         {
@@ -113,7 +106,7 @@ int main() {
                     return crow::response(400);
                 }
                 const std::string outfile_name = params_it->second;
-
+        
                 for (const auto& part_header : part_value.headers)
                 {
                     const auto& part_header_name = part_header.first;
@@ -126,7 +119,7 @@ int main() {
                         CROW_LOG_DEBUG << " Param: " << param_key << ',' << param_val;
                     }
                 }
-
+        
                 // Create a new file with the extracted file name and write file contents to it
                 std::ofstream out_file(outfile_name);
                 if (!out_file)
@@ -141,16 +134,22 @@ int main() {
             else
             {
                 std::istringstream file_ss(part_value.body);
-                
+
+                sql::PreparedStatement* prep_stmt;
+                prep_stmt = con->prepareStatement("INSERT INTO weather (date) VALUES (?)");
+
                 for (std::string file_line; std::getline(file_ss, file_line); )
                 {
-                    CROW_LOG_DEBUG << " Value: " << file_line << '\n';
+                    int i = 0;
 
+                    CROW_LOG_DEBUG << " Value: " << file_line << '\n';
+        
                     std::vector<std::string> weather_line;
                     std::stringstream line_ss(file_line);
                     
                     SWeatherLine weather_line_s;
-                    int i = 0;
+
+                    
                     while (line_ss.good())
                     {
                         std::string column;
@@ -159,188 +158,52 @@ int main() {
                        
                         tm date_time = {};
                         std::time_t date_time_t;
-
+        
                         // Create a string stream to parse the date string
                         std::istringstream date_ss(weather_line[i]);
-
+        
                         switch (i)
                         {
                             case EWeatherTableCols::DATE:
-
+        
                                 // Parse the date string using std::get_time
                                 date_ss >> std::get_time(&date_time, "%Y/%m/%d");
-
+        
                                 // Check if parsing was successful
                                 if (date_ss.fail())
                                 {
                                     CROW_LOG_ERROR << "Date parsing failed!" << '\n';
                                 }
-
+        
                                 // Convert the parsed date to a time_t value
                                 date_time_t = mktime(&date_time);
-
+        
                                 // Output the parsed date using std::asctime
-                                CROW_LOG_INFO << " Value[" << i << "]: " << asctime(localtime(&date_time_t)) << '\n';
-                                weather_line_s.date = asctime(localtime(&date_time_t));
+                                weather_line_s.date = date_time_t;
                                 break;
                             default:
                                 break;
-                        }    
-
+                        }
                         i++;
                     }
+                    char buff[20];
+                    strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&weather_line_s.date));
+                    CROW_LOG_INFO << " Value[" << i << "]: " << buff << '\n';
 
-                    
-                    sqlite3_stmt* stmt;
-                    const char* sql = "INSERT INTO weather (date) VALUES (?)";
-                    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
-                        return crow::response{500, "{\"error\": \"Database error\"}"};
-                    }
-                    
-                    sqlite3_bind_text(stmt, 1, weather_line_s.date.c_str(), -1, SQLITE_STATIC);
-                
-                    if (sqlite3_step(stmt) != SQLITE_DONE) {
-                        sqlite3_finalize(stmt);
-                        return crow::response{500, "{\"error\": \"Failed to insert todo\"}"};
-                    }
+                    prep_stmt->setDateTime(1, sql::SQLString(buff));
+                    prep_stmt->execute();
 
-                    sqlite3_finalize(stmt);
-                }                
+                }
+
+                delete prep_stmt;         
             }
         }
-
-        // auto json = crow::json::load(req.body);
-        // if (!json || !json.has("title")) {
-        //     return crow::response{400, "{\"error\": \"Title is required\"}"};
-        // }
-    //
-    //    std::string title = json["title"].s();
-    //    int completed = 0;
-    //
-    //    if (json.has("completed")) {
-    //        try {
-    //            completed = json["completed"].b() ? 1 : 0;
-    //        } catch (const std::runtime_error& e) {
-    //            if (json["completed"].t() == crow::json::type::Number) {
-    //                completed = json["completed"].i();
-    //            } else {
-    //                return crow::response{400, "{\"error\": \"Completed must be a boolean or a number\"}"};
-    //            }
-    //        }
-    //    }
-    //
-    //    sqlite3_stmt* stmt;
-    //    const char* sql = "INSERT INTO todos (title, completed) VALUES (?, ?)";
-    //    if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) != SQLITE_OK) {
-    //        return crow::response{500, "{\"error\": \"Database error\"}"};
-    //    }
-    //
-    //    sqlite3_bind_text(stmt, 1, title.c_str(), -1, SQLITE_STATIC);
-    //    sqlite3_bind_int(stmt, 2, completed);
-    //
-    //    if (sqlite3_step(stmt) != SQLITE_DONE) {
-    //        sqlite3_finalize(stmt);
-    //        return crow::response{500, "{\"error\": \"Failed to insert todo\"}"};
-    //    }
-    //
-    //    sqlite3_finalize(stmt);
-    //    return crow::response{201, "{\"message\": \"Todo added successfully\"}"};
+   
         return crow::response{201, "{\"message\": \"THIS IS A LIE. Weather added successfully\"}"};
-    });
-    
-    CROW_ROUTE(app, "/weather/<int>")
-    .methods("PUT"_method)([&db](const crow::request& req, int id) {
-    //    auto json = crow::json::load(req.body);
-    //    if (!json || !json.has("completed")) {
-    //        return crow::response{400, "{\"error\": \"Completed status is required\"}"};
-    //    }
-    //
-    //    int completed = 0;
-    //
-    //    if (json.has("completed")) {
-    //        try {
-    //            completed = json["completed"].b() ? 1 : 0;
-    //        } catch (const std::runtime_error& e) {
-    //            if (json["completed"].t() == crow::json::type::Number) {
-    //                completed = json["completed"].i();
-    //            } else {
-    //                return crow::response{400, "{\"error\": \"Completed must be a boolean or a number\"}"};
-    //            }
-    //        }
-    //    }
-    //
-    //    sqlite3_stmt* stmt;
-    //    if (sqlite3_prepare_v2(db, "UPDATE todos SET completed = ? WHERE id = ?", -1, &stmt, nullptr) != SQLITE_OK) {
-    //        return crow::response{500, "{\"error\": \"Database error\"}"};
-    //    }
-    //
-    //    sqlite3_bind_int(stmt, 1, completed);
-    //    sqlite3_bind_int(stmt, 2, id);
-    //
-    //    if (sqlite3_step(stmt) != SQLITE_DONE) {
-    //        sqlite3_finalize(stmt);
-    //        return crow::response{500, "{\"error\": \"Failed to update todo\"}"};
-    //    }
-    //
-    //    sqlite3_finalize(stmt);
-    //    return crow::response{200, "{\"message\": \"Todo updated successfully\"}"};
-        return crow::response{200, "{\"message\": \"THIS IS A LIE. Weather line updated successfully\"}"};
-    });
-
-    CROW_ROUTE(app, "/weather/<int>")
-    .methods("DELETE"_method)([&db](int id) {
-    //    sqlite3_stmt* stmt;
-    //    if (sqlite3_prepare_v2(db, "DELETE FROM todos WHERE id = ?", -1, &stmt, nullptr) != SQLITE_OK) {
-    //        return crow::response{500, "{\"error\": \"Database error\"}"};
-    //    }
-    //
-    //    sqlite3_bind_int(stmt, 1, id);
-    //
-    //    if (sqlite3_step(stmt) != SQLITE_DONE) {
-    //        sqlite3_finalize(stmt);
-    //        return crow::response{500, "{\"error\": \"Failed to delete todo\"}"};
-    //    }
-    //
-    //    sqlite3_finalize(stmt);
-    //    return crow::response{200, "{\"message\": \"Todo deleted successfully\"}"};
-        return crow::response{200, "{\"message\": \"THIS IS A LIE. Weather line deleted successfully\"}"};
-    });
-
-    CROW_ROUTE(app, "/weather/<int>")
-    .methods("GET"_method)([&db](int id) {
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(db, "SELECT "
-            "id, "
-            "date, "
-            "city, "
-            "temp_max, "
-            "temp_min, "
-            "precipitation, "
-            "cloudiness FROM weather WHERE id = ?", -1, &stmt, nullptr) != SQLITE_OK) {
-            return crow::response{500, "{\"error\": \"Database error\"}"};
-        }
-
-        sqlite3_bind_int(stmt, 1, id);
-
-        if (sqlite3_step(stmt) == SQLITE_ROW) {
-            crow::json::wvalue weather_line;
-            weather_line["id"] = sqlite3_column_int(stmt, 0);
-            weather_line["date"] = (const char*)sqlite3_column_text(stmt, 1);
-            weather_line["city"] = (const char*)sqlite3_column_text(stmt, 2);
-            weather_line["temp_max"] = sqlite3_column_double(stmt, 3);
-            weather_line["temp_min"] = sqlite3_column_double(stmt, 4);
-            weather_line["precipitation"] = sqlite3_column_double(stmt, 5);
-            weather_line["cloudiness"] = sqlite3_column_double(stmt, 6);
-            sqlite3_finalize(stmt);
-            return crow::response{200, weather_line};
-        } else {
-            sqlite3_finalize(stmt);
-            return crow::response{404, "{\"error\": \"Weather line not found\"}"};
-        }
     });
 
     app.port(8080).bindaddr("0.0.0.0").multithreaded().run();
 
-    sqlite3_close(db);
+    delete con;
     return 0;
 }
